710d0324705c3a8587b22e02bf141acd8575ed1e
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2025-12-01 12:04:39 +0100

    tests(gha): enable Github Actions (#1)

--- include/hmll/hmll.h
+++ include/hmll/hmll.h
@@ -4,7 +4,22 @@
 extern "C" {
 #endif
 
+#ifndef HMLL_EXTERN
+#ifndef HMLL_STATIC
+#ifdef _WIN32
+#define HMLL_EXTERN __declspec(dllimport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#define HMLL_EXTERN __attribute__((visibility("default")))
+#else
+#define HMLL_EXTERN
+#endif
+#else
+#define HMLL_EXTERN
+#endif
+#endif
+
 #include "status.h"
+
 
 #ifdef __cplusplus
 }
