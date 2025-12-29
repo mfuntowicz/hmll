7103e10cd5c0406eb4eefa2fa9d7398132c596c3
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-12-30 00:15:50 +0100

    test(dtype): cover all the dtypes

--- tests/tests_hmll_dtype.cpp
+++ tests/tests_hmll_dtype.cpp
@@ -7,7 +7,26 @@
 
 TEST_CASE("sizeof dtype", "[dtype]")
 {
-    REQUIRE(hmll_sizeof(HMLL_DTYPE_BFLOAT16) == 2);
-    REQUIRE(hmll_sizeof(HMLL_DTYPE_FLOAT16)  == 2);
-    REQUIRE(hmll_sizeof(HMLL_DTYPE_FLOAT32)  == 4);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_BOOL)            == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_BFLOAT16)        == 16);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_COMPLEX)         == 64);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT4)          == 4);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT6_E2M3)     == 6);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT6_E3M2)     == 6);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT8_E4M3)     == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT8_E5M2)     == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT8_E8M0)     == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT16)         == 16);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_FLOAT32)         == 32);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_SIGNED_INT4)     == 4);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_SIGNED_INT8)     == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_SIGNED_INT16)    == 16);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_SIGNED_INT32)    == 32);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_SIGNED_INT64)    == 64);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNSIGNED_INT4)   == 4);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNSIGNED_INT8)   == 8);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNSIGNED_INT16)  == 16);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNSIGNED_INT32)  == 32);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNSIGNED_INT64)  == 64);
+    REQUIRE(hmll_nbits(HMLL_DTYPE_UNKNOWN)         == 0);
 }
