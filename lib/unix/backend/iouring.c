c7a492c17cf9698ceb4e8a5923bba9052d6ec2d7
Morgan Funtowicz <funtowiczmo@gmail.com>
2026-01-06 15:55:45 +0100

    misc: do not cry if a function is not being used

--- lib/unix/backend/iouring.c
+++ lib/unix/backend/iouring.c
@@ -32,7 +32,6 @@ static struct hmll_error hmll_iouring_register_staging_buffers(
     return HMLL_OK;
 }
 
-#ifdef __HMLL_CUDA_ENABLED__
 /**
  * Checks for completed CUDA events and reclaims the associated io_uring slots.
  * If CUDA is disabled or the device is CPU, this is a no-op.
@@ -41,6 +40,7 @@ static inline void hmll_iouring_reclaim_slots(
     struct hmll_iouring *fetcher,
     const enum hmll_device device
 ) {
+#ifdef __HMLL_CUDA_ENABLED__
     if (device != HMLL_DEVICE_CUDA) return;
 
     struct hmll_iouring_cuda_context *dctx = fetcher->device_ctx;
@@ -55,8 +55,8 @@ static inline void hmll_iouring_reclaim_slots(
             }
         }
     }
-}
 #endif
+}
 
 /**
  * Prepares a single SQE (Submission Queue Entry).
