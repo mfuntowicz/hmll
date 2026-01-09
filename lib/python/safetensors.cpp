f785d9b8cc98d7acd22b9b4a644e38b914218223
Morgan Funtowicz <funtowiczmo@gmail.com>
2026-01-09 14:13:20 +0100

    feat(python): enable fetching contiguous tensors

--- lib/python/safetensors.cpp
+++ lib/python/safetensors.cpp
@@ -1,13 +1,18 @@
 //
 // Created by mfuntowicz on 1/8/26.
 //
+#if defined(__linux__) || defined(__unix__)
+#include <sys/mman.h>
+#endif
 #include <filesystem>
 #include <mutex>
 #include <unordered_map>
-#include <thread>
 #include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
 #include <nanobind/stl/filesystem.h>
+#include <nanobind/stl/string.h>
 #include <hmll/hmll.h>
+#include "ndarray.hpp"
 
 namespace nb = nanobind;
 using namespace nb::literals;
@@ -15,50 +20,114 @@ using namespace nb::literals;
 
 class SafetensorsAccessor
 {
+    hmll_device_t device_;
     std::shared_ptr<hmll_t> base_ctx_;
     std::shared_ptr<hmll_registry_t> registry_;
-    std::shared_ptr<hmll_source_t> source_;
-
-    // Thread-local contexts map: thread_id -> hmll_t
-    mutable std::mutex ctx_map_mutex_;
-    mutable std::unordered_map<std::thread::id, std::unique_ptr<hmll_t>> thread_contexts_;
+    std::shared_ptr<hmll_source_t> sources_;
 
     hmll_t* get_thread_context() const {
-        const auto tid = std::this_thread::get_id();
-        std::lock_guard lock(ctx_map_mutex_);
+        // cache the thread accessing this instance, if single-threaded, will skip all map lookups and stuff
+        thread_local const SafetensorsAccessor* t_last_owner_ = nullptr;
+        thread_local hmll_t* t_last_ctx_ = nullptr;
 
-        auto it = thread_contexts_.find(tid);
-        if (it == thread_contexts_.end()) {
+        if (t_last_owner_ == this) [[likely]]
+            return t_last_ctx_;
+
+        // if another thread is accessing it, let's make sure to use the thread' specific instance (create it if needed)
+        thread_local std::unordered_map<const SafetensorsAccessor*, std::unique_ptr<hmll_t>> t_local_map_;
+
+        hmll_t* ptr = nullptr;
+        if (const auto it = t_local_map_.find(this); it != t_local_map_.end()) {
+            ptr = it->second.get();
+        } else {
             auto ctx = std::make_unique<hmll_t>();
-            if (!hmll_success(hmll_clone_context(base_ctx_.get(), ctx.get()))) {
+            if (!hmll_success(hmll_clone_context(base_ctx_.get(), ctx.get())))
                 return nullptr;
-            }
-            auto* ptr = ctx.get();
-            thread_contexts_[tid] = std::move(ctx);
-            return ptr;
+
+            ptr = ctx.get();
+            t_local_map_[this] = std::move(ctx);
         }
-        return it->second.get();
+
+        t_last_owner_ = this;
+        t_last_ctx_ = ptr;
+        return ptr;
     }
 
 public:
-    explicit SafetensorsAccessor(const std::filesystem::path& path)
-        : base_ctx_(std::make_shared<hmll_t>()), registry_(std::make_shared<hmll_registry_t>()), source_(std::make_shared<hmll_source_t>())
+    explicit SafetensorsAccessor(const std::filesystem::path& path, const hmll_device_t device):
+        device_(device),
+        base_ctx_(std::make_shared<hmll_t>()),
+        registry_(std::make_shared<hmll_registry_t>()),
+        sources_(std::make_shared<hmll_source_t>())
     {
-        if (!hmll_success(hmll_source_open(path.c_str(), source_.get()))) {
+        if (!hmll_success(hmll_source_open(path.c_str(), sources_.get())))
             throw std::runtime_error("Failed to open file: " + path.string());
-        }
 
-        base_ctx_->sources = source_.get();
+
+        base_ctx_->sources = sources_.get();
         base_ctx_->num_sources = 1;
-        base_ctx_->fetcher = nullptr;
-        base_ctx_->error = HMLL_OK;
 
-        if (!hmll_success(hmll_safetensors_populate_registry(base_ctx_.get(), registry_.get(), *source_, 0, 0))) {
-            throw std::runtime_error("Failed to read tensor definition in file " + path.string() + ": " + hmll_strerr(base_ctx_->error));
-        }
+        if (hmll_check(hmll_loader_init(base_ctx_.get(), sources_.get(), 1, device, HMLL_FETCHER_AUTO)))
+            throw std::runtime_error("Failed to allocate loader");
+
+        if (!hmll_success(hmll_safetensors_populate_registry(base_ctx_.get(), registry_.get(), *sources_, 0, 0)))
+            throw std::runtime_error(
+                "Failed to read tensor definition in file " + path.string() + ": " + hmll_strerr(base_ctx_->error));
     }
 
+    hmll_device_t device() const { return device_; }
     size_t size() const { return registry_->num_tensors; }
+
+    nb::ndarray<nb::ndim<1>, nb::c_contig> fetch(const std::string& name) const
+    {
+        auto buffer = std::make_unique<hmll_iobuf_t>();
+        auto offsets = hmll_range_t();
+        auto specs = hmll_tensor_specs_t();
+
+        {
+            nb::gil_scoped_release release;
+            const auto ctx = get_thread_context();
+            const auto registry = registry_.get();
+
+            if (const auto index = hmll_find_by_name(ctx, registry, name.c_str()); index >= 0 && index < registry->num_tensors)
+            {
+                specs = registry->tensors[index];
+                const auto iofile = registry->indexes[index];
+                const auto nbytes = ALIGN_UP(specs.end, 4096) - ALIGN_DOWN(specs.start, 4096);
+
+                // Allocate buffer for the tensor
+                const auto dev = device();
+                buffer->ptr = hmll_get_buffer(ctx, dev, nbytes);
+                buffer->size = nbytes;
+                buffer->device = dev;
+
+                if (!buffer->ptr)
+                    throw std::runtime_error("Failed to allocate buffer");
+
+                // Fetch the tensor data
+                const auto range = hmll_range_t{specs.start, specs.end};
+                offsets = hmll_fetch(ctx, buffer.get(), range, iofile);
+                if (hmll_check(ctx->error)) {
+                    munmap(buffer->ptr, buffer->size);
+                    throw std::runtime_error("Failed to read data");
+                }
+            } else {
+                throw nb::key_error(name.c_str());
+            }
+        }
+
+        // Let's make sure we are not deleting the buffer before PyTorch releases it
+        const hmll_iobuf_t* handle = buffer.release();
+        const nb::capsule deleter(handle, [](void* p) noexcept {
+            if (const auto* b = static_cast<hmll_iobuf_t*>(p)) {
+                munmap(b->ptr, b->size);
+                delete b;
+            }
+        });
+
+        return hmll_to_ndarray({specs.start, specs.end}, *handle, offsets, specs.dtype, deleter);
+
+    }
 };
 
 void init_safetensors(nb::module_& m)
@@ -73,9 +142,11 @@ void init_safetensors(nb::module_& m)
         nb::arg("exc_type").none(),
         nb::arg("exc_value").none(),
         nb::arg("traceback").none()
-    );
+    )
+    .def_prop_ro("device", &SafetensorsAccessor::device)
+    .def("fetch", &SafetensorsAccessor::fetch, "name"_a.sig("str"));
 
-    m.def("safetensors", [](const std::filesystem::path& path) {
-        return new SafetensorsAccessor(path);
+    m.def("safetensors", [](const std::filesystem::path& path, const hmll_device_t device) {
+        return new SafetensorsAccessor(path, device);
     }, nb::rv_policy::take_ownership);
 }
